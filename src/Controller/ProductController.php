98ec980ce540eeb77a13fa39656b9f4d3a74552d
thibaud <thibaud.mallet@hotmail.fr>
2023-04-01 21:00:39 +0200

    Ajout de l'obligation d'etre authentifié pour accéder à la route product list

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,13 +3,15 @@
 namespace App\Controller;
 
 use App\Repository\ProductsRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
     #[Route('/product/list', name: 'app_product_list')]
+    #[IsGranted('ROLE_USER')]
     public function list(ProductsRepository $productsRepository): Response
     {
         $products = $productsRepository->findAll();
